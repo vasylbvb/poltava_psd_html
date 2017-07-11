19c0dffd53c546400a9cde391dc831aa53fd8ec6
vasylbvb <basebv@gmail.com>
2017-07-12 02:43:02 +0300

    simple slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
 
 $(document).ready(function () {
-
+    $('.simple-slider').bxSlider({
+        slideWidth: 270,
+        minSlides: 1,
+        maxSlides: 4,
+        moveSlides: 1,
+        slideMargin: 30,
+        pager: false,
+        prevText: '',
+        nextText: '',
+        prevSelector: '.simple-slider__arrow-prev',
+        nextSelector: '.simple-slider__arrow-next'
+    });
 });
