e33f926ffed6eaf327e72408679b430b8659205d
vasylbvb <basebv@gmail.com>
2017-07-17 11:57:17 +0300

    updated form validation

--- src/js/main.js
+++ src/js/main.js
@@ -126,4 +126,93 @@ $(document).ready(function () {
     totalPriceOutput.setAttribute("value", totalRes);
     }
 
+
+    var form = document.querySelector(".validate-this");
+    (function () {
+        var textName = form.name,
+            textEmail = form.emailAddress,
+            textProject = form.projectTitle;
+
+        form.addEventListener("submit", validate);
+
+        function validate(e) {
+            e.preventDefault();
+
+            var form = this,
+                inputs = form.querySelectorAll("[data-error]");
+
+            clear(inputs, function () {
+                checkIsEmpty(inputs);
+            });
+
+            function checkIsEmpty(inputs) {
+                var isEmpty = false;
+
+                for (var i = 0; i < inputs.length; i++) {
+                    var input = inputs[i];
+
+                    if (input.value.trim() === "") {
+                        isEmpty = true;
+                        markInput(input);
+                    }
+                }
+
+                if (!isEmpty) {
+                    setTimeout(function () {
+                        alert("checking is complete");
+                        form.submit();
+                    }, 300);
+                }
+
+            }
+
+            function markInput(input) {
+                input.classList.add("error");
+                var text = input.getAttribute("data-error");
+
+                if (!text) return;
+
+                var div = document.createElement("div");
+
+                div.textContent = text;
+                div.className = "error-text";
+                input.parentNode.appendChild(div);
+            }
+
+            function clear(inputsItem, callback) {
+                for (var i = 0; i < inputsItem.length; i++) {
+                    var input = inputsItem[i],
+                        parent = input.parentNode,
+                        messagerr = parent.querySelector(".error-text");
+
+                    input.classList.remove("error");
+                    if (messagerr) parent.removeChild(messagerr);
+                }
+
+                if (callback) callback();
+            }
+        }
+    }());
+    form.addEventListener("submit", function(){
+        event.preventDefault();
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/feedback");
+        xhr.setRequestHeader("Content-Type", "application/json")
+        xhr.onreadystatechange = function(){
+            if(this.readyState !==4){
+                return;
+            }
+            var out = document.createElement("div");
+            var jsonString = this.responseText;
+            out.innerText = jsonString;
+            document.body.appendChild(out);
+        };
+        var data ={
+            name: "",
+            emailAddress: "",
+            projectTitle: ""
+        };
+        var str = JSON.stringify(data);
+        xhr.send(str);
+    });
 });
